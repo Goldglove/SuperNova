63cf3c0bb0482c5ea1aedc9438ac20040ef12465
Kian Shepherd <kianshepherd29@gmail.com>
2015-03-12 18:58:56 -0400

    Fixed errors and got drawing fixed

--- Main.py
+++ Main.py
@@ -1,22 +1,39 @@
-import pygame
+import pygame as pg
+import sys, os
 
 game_running = True
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+darkBlue = (0,0,128)
+white = (255,255,255)
+black = (0,0,0)
+pink = (255,200,200)
+
+pg.init()
+"""Create the Screen"""
+screen = pg.display.set_mode((600, 480))
 
 class main:
     """The Main PyMan Class - This class handles the main 
     initialization and creating of the Game."""
-    
-	def __init__(self, width=640,height=480):
-		pygame.init()
-        """Set the window Size"""
-		self.width = width
-		self.height = height
-        """Create the Screen"""
-		self.screen = pygame.display.set_mode((self.width, self.height))
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption("SuperNova")
+
+    def Draw():
+        screen.fill(white)
+        pg.draw.line(screen, black, (100, 100), (300, 300), 5)
+        
 
 if __name__ == "__main__":
-	while game_running:	
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: 
-				sys.exit()
-		main()
+    main()
+    while game_running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+        main.Draw()
+        pg.display.flip() 
+        pg.time.delay(60)
+        
