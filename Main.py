514fb511a93001908a206af4e48c72480ef7b18c
ArcanianArcher <arcanianarcher@gmail.com>
2015-03-13 10:29:41 -0400

    Pictures and window icon
    I added some folders for pictures, along with a couple pictures. Still
    need to make the internal views for the ships. Also need to find more
    ships in general.
    
    I also added in an icon, and the code to display the icon was added to
    main.py
    
    The icon still has watermarks... :/
    I'll fix it later.

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import pygame as pg
 import sys, os
 
 game_running = True
+
+#Colour Dictionary
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
@@ -11,21 +13,22 @@ black = (0,0,0)
 pink = (255,200,200)
 
 pg.init()
-"""Create the Screen"""
+#Create the Screen
 screen = pg.display.set_mode((600, 480))
 
+#The Main Class - This class handles the main initialization and creating of the Game.
 class main:
-    """The Main PyMan Class - This class handles the main 
-    initialization and creating of the Game."""
     def __init__(self):
         pg.init()
         pg.display.set_caption("SuperNova")
+        window_icon_path = os.getcwd() + '\\Images\\window_icon.jpg'
+        window_icon = pg.image.load(window_icon_path)
+        pg.display.set_icon(window_icon)
 
     def Draw():
         screen.fill(white)
         pg.draw.line(screen, black, (100, 100), (300, 300), 5)
         
-
 if __name__ == "__main__":
     main()
     while game_running:
@@ -36,4 +39,3 @@ if __name__ == "__main__":
         main.Draw()
         pg.display.flip() 
         pg.time.delay(60)
-        
