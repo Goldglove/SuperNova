fe16c6365f20601fd0d1ad1902f85b19ade995b8
Kian Shepherd <kianshepherd29@gmail.com>
2015-03-24 18:26:57 -0400

    wat

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame as pg
 import GameObject, Weapons, Player, Enemy
 import sys, os
@@ -81,17 +80,4 @@ if __name__ == "__main__":
             main.Update()
             main.Draw()
          
-
-=======
-#Main class for the game... Will work on gettign multiple files with classes working tomorrow.
-class SuperNova:
-    #Different functions and methods in main class.
-    def printHello():
-        return ("Hello World!")
-
-#Where the main loop will be in the game.
-if __name__ == "__main__":
-    #Supernova tells which class to take from .printHello tells what function in the class to use.
-    print(SuperNova.printHello())
->>>>>>> origin/master
 
