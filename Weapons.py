8bb5ea3abec9a7a3919a3a2231925b2d7d1b897b
Kian Shepherd <kianshepherd29@gmail.com>
2015-03-24 17:02:40 -0400

    commiting

--- Weapons.py
+++ Weapons.py
@@ -1,12 +1,26 @@
 import Main, os, GameObject
+import pygame as pg
 
 class Weapons():
     global x
     global y
+    global screen
+    global game_object
+    global num_slides
     
-    def __init__(self, pic_name, Screen, pygame):
-        self.SelF = GameObject.GameObject(pic_name, Screen, pygame)
-        return
+    def __init__(self, pic_name):
+        global game_object
+        global x
+        global y
+        ship_path = os.getcwd() + '\\Images\\Weapons\\' + pic_name
+        game_object = pg.image.load(ship_path)
+        x = 20
+        y = 0
+        num_slides = 6
+        slide_on = 1
 
+    def Animate():
+        slide on += 1
+        
     def Draw():
-        self.SelF.Draw()
+        screen.blit(game_object, (x, y))
