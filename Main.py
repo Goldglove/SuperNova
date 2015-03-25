26032b89ee551aee3b14ca72419f7133ffe07474
Kian Shepherd <kianshepherd29@gmail.com>
2015-03-25 16:02:09 -0400

    reading from weapon data done

--- Main.py
+++ Main.py
@@ -4,6 +4,9 @@ import sys, os
 
 game_running = True
 
+wep_data = open("weapon.data", 'r')
+wep_data.readline()
+
 #Colour Dictionary
 red = (255,0,0)
 green = (0,255,0)
@@ -22,8 +25,8 @@ pause = 1
 #The Main Class - This class handles the main initialization and creating of the Game.
 class main:
     global player1
-    global weapon1
     global enemy1
+    global weapon
     def __init__(self):
         pg.init()
         pg.display.set_caption("SuperNova")
@@ -31,29 +34,49 @@ class main:
         window_icon = pg.image.load(window_icon_path)
         pg.display.set_icon(window_icon)
         global player1
-        global weapon1
+        global weapon
         global enemy1
-        global weapon2
+        weapon = []
         Weapons.Weapons.InitArrays()
         enemy1 = Enemy.Enemy()
         #(pic name, slide width, slide height, num slides)
-        weapon1 = Weapons.Weapons.CreateWeapon("autocannon3.png", 20, 79, 6, 0, 3)
-        weapon2 = Weapons.Weapons.CreateWeapon("wep1.png", 26, 75, 7, 0, 12)
         player1 = Player.Player()
 
     def Draw():
         screen.fill(white)
         Player.Player.Draw(screen)
         Enemy.Enemy.Draw(screen)
-        Weapons.Weapons.Draw(screen, 20, 0, 1)
-        Weapons.Weapons.Draw(screen, 20, 200, 2)
+        Weapons.Weapons.Draw(screen, 20, 0, 3)
+        Weapons.Weapons.Draw(screen, 20, 200, 7)
+        Weapons.Weapons.Draw(screen, 20, 400, 1)
         pg.time.delay(60)
         pg.display.flip()
 
+    def LoadWeapon():
+    #will add the reverse animate during tomorros class
+        i = 0
+        for line in wep_data:
+            i += 1
+            temp_string = line
+            temp_array = []
+            temp_array = temp_string.split(' ')
+            ship_name = temp_array[0]
+            slide_width = temp_array[1]
+            slide_height = temp_array[2]
+            num_slides = temp_array[3]
+            attack_dmg = temp_array[4]
+            attack_spd = temp_array[5]
+            power_req = temp_array[6]
+            num_shots = temp_array[7]
+            use_missle = temp_array[8]
+            weapon.append(Weapons.Weapons.CreateWeapon(ship_name, int(slide_width), int(slide_height), int(num_slides), int(attack_dmg), int(attack_spd)))
+        
+        
     def Update():
         Weapons.Weapons.Animate()
         
     def Quit():
+        wep_data.close()
         pg.quit()
         sys.exit()
     def pause():
@@ -78,10 +101,11 @@ class main:
         
 if __name__ == "__main__":
     main()
+    main.LoadWeapon()
     while game_running:
         main.InputEvents()
         if pause % 2 == 1:
             main.Update()
             main.Draw()
-         
+    
 
